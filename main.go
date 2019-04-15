a486dbb737abf9cacd9c61474f082c3024449444
Maren Beam <maren@chro.bid>
2019-04-15 11:54:06 -0400

    start command parsing stuff. This could change drastically -- i can foresee the current setup getting super convoluted with many commands, but i don't currently know what a better way would be.

--- main.go
+++ main.go
@@ -10,18 +10,28 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-
 	fmt.Println("Hello, wanderer. This is Claster.")
+
 	for {
 		fmt.Print(`> `)
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			log.Panic(err)
 		}
+		input = strings.TrimSuffix(input, "\n")
 		if strings.HasPrefix(input, `/`) {
-			fmt.Println("This might one day be a command.")
-		} else {
-			fmt.Print(input)
+			command(input)
 		}
+	}
+}
+
+func command(input string) {
+	switch input {
+	case `/quit`:
+		os.Exit(0)
+	case `/exit`:
+		os.Exit(0)
+	default:
+		fmt.Println("Sorry, I don't understand.")
 	}
 }
