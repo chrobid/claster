6e304049e264e496e551be5f7d609c10bdd8367e
Maren Beam <maren@chro.bid>
2019-04-15 09:15:18 -0400

    it's small, but it's progress! and i haven't even had lunch yet wooo

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Hello, wanderer. This is Claster.")
+	for {
+		fmt.Print(`> `)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Panic(err)
+		}
+		if strings.HasPrefix(input, `/`) {
+			fmt.Println("This might one day be a command.")
+		} else {
+			fmt.Print(input)
+		}
+	}
 }
